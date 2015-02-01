6b107d6e7fe3020448776a0821780b71373e0bf5
Ressk <mitchellarthurcraig@gmail.com>
2015-02-01 16:55:40 -0600

    yoho

--- RunningManUnity/Assets/Scripts/LevelScripts/GoToMenu.cs
+++ RunningManUnity/Assets/Scripts/LevelScripts/GoToMenu.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using UnityEngine.UI;
 
 public class GoToMenu : MonoBehaviour {
 
 	void Update () {
-	    if (Input.GetKeyDown(KeyCode.Enter))
+	    if (Input.GetKeyDown(KeyCode.Return))
         {
             Application.LoadLevel("Menu");
         }
