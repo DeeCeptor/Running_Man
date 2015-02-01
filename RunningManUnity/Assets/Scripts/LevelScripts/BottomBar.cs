10c7ee761218e1863088525bea3cbe0e9476cf5a
Ressk <mitchellarthurcraig@gmail.com>
2015-02-01 10:54:20 -0600

    smack

---
+++ RunningManUnity/Assets/Scripts/LevelScripts/BottomBar.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BottomBar : MonoBehaviour
+{
+    public float damageOnHit = 100;
+    public float heightReset = 100;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Runner")
+        {
+            coll.gameObject.SendMessage("TakeHit", damageOnHit);
+            coll.gameObject.transform.position += new Vector3(0, heightReset, 0);
+        }
+    }
+}
