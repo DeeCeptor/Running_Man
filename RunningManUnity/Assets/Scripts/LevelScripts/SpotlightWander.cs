b1ccfad47c7497c210b83d93cb04b51a261e9aba
Ressk <mitchellarthurcraig@gmail.com>
2015-01-31 18:37:24 -0600

    boom

---
+++ RunningManUnity/Assets/Scripts/LevelScripts/SpotlightWander.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpotlightWander : MonoBehaviour {
+
+    Random random;
+
+	// Use this for initialization
+	void Start () {
+        random = new Random();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    transform.rotation = new Vector3(
+	}
+}
