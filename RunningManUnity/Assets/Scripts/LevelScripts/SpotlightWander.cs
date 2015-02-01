83246cde1f30c558d096c5de56b6915db757d242
Ressk <mitchellarthurcraig@gmail.com>
2015-01-31 18:51:25 -0600

    wandering lights!

--- RunningManUnity/Assets/Scripts/LevelScripts/SpotlightWander.cs
+++ RunningManUnity/Assets/Scripts/LevelScripts/SpotlightWander.cs
@@ -3,15 +3,13 @@ using System.Collections;
 
 public class SpotlightWander : MonoBehaviour {
 
-    Random random;
 
 	// Use this for initialization
 	void Start () {
-        random = new Random();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    transform.rotation = new Vector3(
+        transform.Rotate(new Vector3(1,1,1));
 	}
 }
